7114a8fe51a8869b9bb2a601898f8a774c68b977
Jean Carlos <dev.jeancotillo@gmail.com>
2024-04-02 17:12:55 -0300

    consertando bug

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+    
 using InfnetMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -37,6 +38,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
 
 app.Run();
